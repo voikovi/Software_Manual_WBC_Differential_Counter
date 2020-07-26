9f08eac07a14b7651e529bc686912d0f235052d0
Stefan Voykov <62034634+voikovi@users.noreply.github.com>
2020-07-26 15:40:00 +0300

    Add files via upload

---
+++ table_c_console.c
@@ -0,0 +1,67 @@
+#define VERSION " BOXCHAR  Ver 2.1        18-Jun-93        WildeWare for DOS & OS2 Family Mode"
+#include "stdio.h"
+void main(void);
+void main()
+{
+    printf("%s\n\n", VERSION);
+ 
+    printf(" %c  %c  %c    ",     218, 194, 191);
+    printf(" %d %d %d          ", 218, 194, 191);
+    printf(" %c  %c  %c    ",     213, 209, 184);
+    printf(" %d %d %d  \n\n",     213, 209, 184);
+    printf(" %c  %c  %c    ",     195, 197, 180);
+    printf(" %d %d %d          ", 195, 197, 180);
+    printf(" %c  %c  %c    ",     198, 216, 181);
+    printf(" %d %d %d  \n\n",     198, 216, 181);
+    printf(" %c  %c  %c    ",     192, 193, 217);
+    printf(" %d %d %d          ", 192, 193, 217);
+    printf(" %c  %c  %c    ",     212, 207, 190);
+    printf(" %d %d %d  \n\n",     212, 207, 190);
+    printf(" \n\n");
+ 
+ 
+    printf(" %c  %c  %c    ",     214, 210, 183);
+    printf(" %d %d %d          ", 214, 210, 183);
+    printf(" %c  %c  %c    ",     201, 203, 187);
+    printf(" %d %d %d  \n\n",     201, 203, 187);
+    printf(" %c  %c  %c    ",     199, 215, 182);
+    printf(" %d %d %d          ", 199, 215, 182);
+    printf(" %c  %c  %c    ",     204, 206, 185);
+    printf(" %d %d %d  \n\n",     204, 206, 185);
+    printf(" %c  %c  %c    ",     211, 208, 189);
+    printf(" %d %d %d          ", 211, 208, 189);
+    printf(" %c  %c  %c    ",     200, 202, 188);
+    printf(" %d %d %d  \n\n",     200, 202, 188);
+    printf(" \n\n");
+ 
+    printf("    %c  %c  ",        196, 179);
+    printf(" %d %d             ", 196, 179);
+    printf("    %c  %c  ",        205, 186);
+    printf(" %d %d  \n\n",        205, 186);
+    printf(" \n\n");
+ 
+    printf(" %c%c%c%c%c%c%c    ", 218, 196, 196, 194, 196, 196, 191);
+    printf(" %c%c%c%c%c%c%c    ", 213, 205, 205, 209, 205, 205, 184);
+    printf(" %c%c%c%c%c%c%c    ", 214, 196, 196, 210, 196, 196, 183);
+    printf(" %c%c%c%c%c%c%c    ", 201, 205, 205, 203, 205, 205, 187);
+    printf(" \n");
+    printf(" %c  %c  %c    ",   179, 179, 179);
+    printf(" %c  %c  %c    ",   179, 179, 179);
+    printf(" %c  %c  %c    ",   186, 186, 186);
+    printf(" %c  %c  %c    ",   186, 186, 186);
+    printf(" \n");
+    printf(" %c%c%c%c%c%c%c    ", 195, 196, 196, 197, 196, 196, 180);
+    printf(" %c%c%c%c%c%c%c    ", 198, 205, 205, 216, 205, 205, 181);
+    printf(" %c%c%c%c%c%c%c    ", 199, 196, 196, 215, 196, 196, 182);
+    printf(" %c%c%c%c%c%c%c    ", 204, 205, 205, 206, 205, 205, 185);
+    printf(" \n");
+    printf(" %c  %c  %c    ",   179, 179, 179);
+    printf(" %c  %c  %c    ",   179, 179, 179);
+    printf(" %c  %c  %c    ",   186, 186, 186);
+    printf(" %c  %c  %c    ",   186, 186, 186);
+    printf(" \n");
+    printf(" %c%c%c%c%c%c%c    ", 192, 196, 196, 193, 196, 196, 217);
+    printf(" %c%c%c%c%c%c%c    ", 212, 205, 205, 207, 205, 205, 190);
+    printf(" %c%c%c%c%c%c%c    ", 211, 196, 196, 208, 196, 196, 189);
+    printf(" %c%c%c%c%c%c%c    ", 200, 205, 205, 202, 205, 205, 188);
+}
